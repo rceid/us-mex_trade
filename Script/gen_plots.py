0141c8528922ae7186c1482f1bf3c63e7a3b2766
Raymond Eid <reid7@uchicago.edu>
2020-07-07 12:14:17 -0500

    fixing bugs, all clean

--- Script/gen_plots.py
+++ Script/gen_plots.py
@@ -14,13 +14,22 @@ plt.rcParams.update({'figure.max_open_warning': 0})
 STATS = ["Mexican Pop", "Latino Pop", "Total Pop"]
 COLS = ['Name', 'Namelsad', 'geometry', 'Mexican Pop', 
             'Latino Pop', 'Total Pop', 'Exports to Mexico, 2018 (USD Million)',
-            'Total Jobs, 2018', 'Representative']
+            'Total Jobs, 2018', 'Representative', 'Party Affiliation']
 
+
+def select_delim(command_line=True):
+    '''
+    '''
+    if command_line:
+        return '/'
+    else:
+        return '\\' 
 
 def go():
-    all_states()
+    delim = select_delim(command_line=True)
+    all_states(COLS, delim)
     
-def plot_state(df, state, stat, path):
+def plot_state(df, state, stat, path, delim):
     '''
     creates a plot by state of a given statistic and saves the image into
     its respective folder
@@ -38,12 +47,12 @@ def plot_state(df, state, stat, path):
     plt.title(stat + " by Congressional District", fontsize=10)
     plt.axis('off')
     file_name = state + '.png'
-    plt.savefig(path + '/' + file_name)
+    plt.savefig(path + delim + file_name)
     plt.close(fig)
     
     return state_df
     
-def plot_district(state_df, district, path):
+def plot_district(state_df, district, path, delim):
     '''
     Creates a plot of the shape of a congressional district
     '''
@@ -52,26 +61,26 @@ def plot_district(state_df, district, path):
     district_plot.plot(edgecolor='black', cmap='Blues_r')
     plt.axis('off')
     file_name = district_plot['Namelsad'].item()
-    plt.savefig(path + '/' + file_name)
+    plt.savefig(path + delim + file_name)
     plt.close(fig)
     
-def all_states():
+def all_states(cols, delim):
     '''
     iteratively creates plots for all states for all stats
     '''
-    tab_df = generate_inputs.prepare_df(COLS)
+    tab_df = generate_inputs.prepare_df(COLS, delim)
     for idx, stat in enumerate(STATS):
-        path = '../Data/' + stat + '_maps'
+        path = '..'+ delim + 'Data' + delim + stat + '_maps'
         if not os.path.exists(path):
             os.mkdir(path)
         for State in tab_df['Name'].unique():
-            state_df = plot_state(tab_df, State, stat, path)
+            state_df = plot_state(tab_df, State, stat, path, delim)
             if idx == 0:
                 for dist in state_df['Namelsad'].unique():
-                    p = '../Data/District_shapes'
+                    p = '..' + delim + 'Data'+ delim + 'District_shapes'
                     if not os.path.exists(p):
                         os.mkdir(p)
-                    plot_district(state_df, dist, p)
+                    plot_district(state_df, dist, p, delim)
                     
                     
                     
--- Script/generate_inputs.py
+++ Script/generate_inputs.py
@@ -13,44 +13,45 @@ import requests
 import zipfile
 import io
 import os 
-import jellyfish
-from us import states as st
+#import jellyfish
+#from us import states as st
 
 pd.options.mode.chained_assignment = None
 
+
 SHAPE_URL = 'https://www2.census.gov/geo/tiger/TIGER2018/CD/tl_2018_us_cd116.zip'
+#cols = ['Name', 'Namelsad', 'geometry', 'Mexican Pop', 'Latino Pop', 'Total Pop', 'Exports to Mexico, 2018 (USD Million)','Total Jobs, 2018', 'Representative', 'Party Affiliation']
 
-#cols = ['Name', 'Namelsad', 'geometry', 'Mexican Pop', 'Latino Pop', 'Total Pop', 'Exports to Mexico, 2018 (USD Million)','Total Jobs, 2018', 'Representative']
-
-def prepare_df(cols):
-    all_data = merge_clean()
-    all_data = trim_alaska(all_data[cols])
+def prepare_df(cols, delim):
+    all_data = merge_clean(delim)
     all_data.drop(['geometry'], axis=1)\
-        .to_csv('../Data/factsheet_data.csv')
+        .to_csv('..' + delim + 'Data' + delim + 'factsheet_data.csv')
+        
     return all_data
 
-def merge_clean():
+def merge_clean(delim):
     '''
     '''
-    state, shape, census, export = clean_dfs()
+    state, shape, census, export = clean_dfs(delim)
     main_df = merge_dfs(state, shape, census, export)
     main_df['Namelsad'] = main_df['Name'] + \
         main_df['Namelsad'].apply(lambda row: format_district(row, True))
-    return main_df 
 
-def clean_dfs():
+    return main_df
+
+def clean_dfs(delim):
     '''
     '''
-    exports = pd.read_excel("../Data/Mexico_exports.xls")
+    exports = pd.read_excel('..' + delim + 'Data' + delim + 'Mexico_exports.csv')
     census = census_scripts.get_census_data()
-    states = pd.read_excel('../Data/state-geocodes-v2016.xls', header=5)
-    shape = get_districts()
+    states = pd.read_excel('..' + delim + 'Data' + delim + 'state-geocodes-v2016.xls', header=5)
+    shape = get_districts(delim)
     shape.rename(columns={"STATEFP": 'State (FIPS)', "NAMELSAD":'Namelsad'}, 
                  inplace=True)
     shape = shape[shape['Namelsad'] != 'Congressional Districts not defined']
     shape['State (FIPS)'] = shape['State (FIPS)'].astype(int)
     exports = clean_exports(exports)
-    
+
     return states, shape, census, exports
 
 def clean_exports(exports_df):
@@ -69,10 +70,12 @@ def clean_exports(exports_df):
     exports_df['Namelsad'] = exports_df['Namelsad']\
         .apply(lambda x: str(x) if len(str(x)) > 1 else '0' + str(x))
     exports_df['Namelsad'] = exports_df['Namelsad'].apply(format_district)
+    #obtained political parties of candidates via string matching with a database online
+    #function was imperfect so I added non matches by hand. Results are
+    #stored directly in the Mexico_exports.csv file and were loaded directly
+    #exports_df = get_party(exports_df)
     
     return exports_df
-
-
 
 def format_district(row, to_state=False):
     '''
@@ -112,18 +115,18 @@ def merge_dfs(state_df, shape_df, census_df, export_df):
     
     return gpd.GeoDataFrame(all_data)
     
-def get_districts():
+def get_districts(delim):
     '''
     '''
     r = requests.get(SHAPE_URL)
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    shape_folder = '../Data/shapefile'
+    shape_folder = '..' + delim + 'Data' + delim + 'shapefile'
     if not os.path.exists(shape_folder):
         os.mkdir(shape_folder)
     z.extractall(path=shape_folder)
     [shapefile] =  [f for f in z.namelist() if f.endswith('.shp')]
 
-    return gpd.read_file(shape_folder + '/' + shapefile)
+    return gpd.read_file(shape_folder + delim + shapefile)
     
 def trim_alaska(all_data):
     '''
@@ -146,37 +149,37 @@ def trim_alaska(all_data):
         
     return states_trimmed
     
-##string cleaning function, partially used then went in by hand:
-    
-def get_party(exports_df):
-    '''
-    ''' 
-    similarity = lambda s1, s2: jellyfish.jaro_winkler(s1, s2)
-    party_df = \
-        pd.read_csv\
-            ("https://theunitedstates.io/congress-legislators/legislators-current.csv")
-    party_df['full_name'] = \
-        np.where(party_df['full_name'].isna(),\
-                 party_df['ballotpedia_id'], party_df['full_name'])  
-    party_df = party_df[party_df['type'] == 'rep']
-    name_change = {'Brian Higgins': 'Chris Jacobs', }
-    for prev, current in name_change.items():
-        party_df.loc[party_df.first_name == prev, \
-                 ['full_name', 'last_name']] = current[0], current[1]
-    party_df['state'] = party_df['state'].apply(lambda row: str(st.lookup(row)))
-    
-    exports_df['Party Affiliation'] = 'No match'
-    for _, party in party_df.iterrows():
-        for _, rep in exports_df.iterrows():
-            name = rep['Representative']
-            if name == 'Rodney Davis' and party['full_name'] == 'Rodney Davis':
-                print(similarity(party['last_name'].split()[-1], \
-                          rep['Apellido'].split()[-1]) )
-            if similarity(party['last_name'].split()[-1], \
-                          rep['Apellido'].split()[-1]) > 0.94\
-                and party['state'] == rep['Name']:
 
-                exports_df.loc[exports_df.Representative == name, \
-                               'Party Affiliation'] = party['party']
-                break    
-    return party_df
+# def get_party(exports_df):
+#    '''
+#    string cleaning function, partially used then went in by hand:
+#    '''
+#     similarity = lambda s1, s2: jellyfish.jaro_winkler(s1, s2)
+#     party_df = \
+#         pd.read_csv\
+#             ("https://theunitedstates.io/congress-legislators/legislators-current.csv")
+#     party_df['full_name'] = \
+#         np.where(party_df['full_name'].isna(),\
+#                  party_df['ballotpedia_id'], party_df['full_name'])  
+#     party_df = party_df[party_df['type'] == 'rep']
+#     name_change = {'Brian Higgins': 'Chris Jacobs', }
+#     for prev, current in name_change.items():
+#         party_df.loc[party_df.first_name == prev, \
+#                  ['full_name', 'last_name']] = current[0], current[1]
+#     party_df['state'] = party_df['state'].apply(lambda row: str(st.lookup(row)))
+    
+#     exports_df['Party Affiliation'] = 'No match'
+#     for _, party in party_df.iterrows():
+#         for _, rep in exports_df.iterrows():
+#             name = rep['Representative']
+#             if name == 'Rodney Davis' and party['full_name'] == 'Rodney Davis':
+#                 print(similarity(party['last_name'].split()[-1], \
+#                           rep['Apellido'].split()[-1]) )
+#             if similarity(party['last_name'].split()[-1], \
+#                           rep['Apellido'].split()[-1]) > 0.94\
+#                 and party['state'] == rep['Name']:
+
+#                 exports_df.loc[exports_df.Representative == name, \
+#                                'Party Affiliation'] = party['party']
+#                 break    
+#     return party_df

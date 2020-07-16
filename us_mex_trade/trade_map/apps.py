3fbe995936a8dab5c368f1d41c46178afd25a262
Raymond Eid <reid7@uchicago.edu>
2020-07-16 12:07:47 -0500

    migrated django files

--- us_mex_trade/trade_map/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TradeMapConfig(AppConfig):
-    name = 'trade_map'
--- us_mex_trade/trade_map/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return render(request, 'trade_map/home.html')
-    
-def about(request):
-    return HttpResponse('<h2> About the map</h2>')
